62eadcfc9f5f5172d4048829f5f2e57e9e1dc373
Karoly Gossler <connor@connor.hu>
2024-03-06 09:15:24 +0100

    refactor: generate modern str functions str_contains

--- generator/lib/builder/om/PHP5ObjectBuilder.php
+++ generator/lib/builder/om/PHP5ObjectBuilder.php
@@ -1019,7 +1019,7 @@ abstract class " . $this->getClassname() . " extends " . $parentClass . " ";
         $script .= "
         }
 
-        if (strpos(\$format, '%') !== false) {
+        if (str_contains(\$format, '%')) {
             @trigger_error(\"Use of strftime format is deprecated in method: \".__CLASS__.'::'.__METHOD__, \\E_USER_DEPRECATED);
             return strftime(\$format, \$dt->format('U'));
         }
